e6643dcbc08c3f6b732ff4589971ef647f0341b4
himanshu-patill <himanshu.patil@necsws.com>
2022-08-01 21:15:34 +0530

    Review-2

---
+++ CountErrorAsync/AsyncClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountErrorAsync
+{
+    internal class AsyncClass
+    {
+
+        public static async Task ReadTheFile(string Files)
+        {
+            int Count = await CountOccurence(Files);
+            Console.WriteLine("{0} contains : {1}", Files, Count);
+
+        }
+
+        public static async Task DeleteTheFile(string Files)
+        {
+            int Count = await CountOccurence(Files);
+            if (Count > 50)
+            {
+                //Console.WriteLine("*****for {0} got count={1}", Files,Count);
+                File.Delete(Files);
+                Console.WriteLine("ATTENTION!! File {0} -- is deleted with [ERROR] Count = {1}", Files, Count);
+
+            }
+            else if (Count > 20 && Count < 50)
+            {
+                System.IO.File.WriteAllText(Files, string.Empty);
+            }
+        }
+
+        public static async Task<int> CountOccurence(string Files)
+        {
+            string[] lines = File.ReadAllLines(Files);
+            //Console.WriteLine(text);
+
+            int Count = 0;
+
+            foreach (var line in lines)
+            {
+                //Console.WriteLine(line);
+                if (line.Contains("[Error]"))
+                {
+                    Count += 1;
+                }
+                //break;
+
+
+            }
+            return Count;
+        }
+        
+            
+        
+
+
+
+    }
+}
--- CountErrorAsync/Program.cs
+++ CountErrorAsync/Program.cs
@@ -1,53 +1,51 @@
-﻿public class Program
+﻿using CountErrorAsync;
+
+public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        string File1 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220715_003.log";
-        string File2 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220715_004.log";
-        string File3 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220715_005.log";
-        string File4 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220716.log";
-        string File5 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220716_001.log";
-        string File6 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220717.log";
-        string File7 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220717_001.log";
-        string File8 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220717_002.log";
-        string File9 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220720.log";
-        string File10 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220728.log";
 
-        ReadTheFile(File1);
-        ReadTheFile(File2);
-        ReadTheFile(File3);
-        ReadTheFile(File4);
-        ReadTheFile(File5);
-        ReadTheFile(File6);
-        ReadTheFile(File7);
-        ReadTheFile(File8);
-        ReadTheFile(File9);
-        ReadTheFile(File10);
+        //string File1 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220715_003.log";
+        //string File2 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220715_004.log";
+        //string File3 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220715_005.log";
+        //string File4 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220716.log";
+        //string File5 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220716_001.log";
+        //string File6 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220717.log";
+        //string File7 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220717_001.log";
+        //string File8 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220717_002.log";
+        //string File9 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220720.log";
+        //string File10 = @"C:\Users\himanshu.patil\Downloads\ErrorLogs\NexusService-20220728.log";
 
-        
-    }
 
-    static async void ReadTheFile(string GivenFile)
-    {
-        string[] lines = File.ReadAllLines(GivenFile);
-        //Console.WriteLine(text);
+        //Reading Files
+        string[] FilePaths = Directory.GetFiles(@"C:\Users\himanshu.patil\Downloads\ErrorLogs\", "*.log");
 
-        int Count = 0;
-
-        await Task.Run(() =>
+        foreach (string Files in FilePaths)
         {
-            foreach (var line in lines)
-            {
-                //Console.WriteLine(line);
-                if (line.Contains("[Error]"))
-                {
-                    Count += 1;
-                }
-                //break;
-            }
-            Console.WriteLine("{0} contains : {1}", GivenFile, Count);
-        });
+            //Console.WriteLine(Files);
+
+
+            await AsyncClass.ReadTheFile(Files);
+            await AsyncClass.DeleteTheFile(Files);
+        }
+
+        //AsyncClass obj = new AsyncClass();
+
+        //obj.ReadTheFile(File1);
+        //obj.ReadTheFile(File2);
+        //obj.ReadTheFile(File3);
+        //obj.ReadTheFile(File4);
+        //obj.ReadTheFile(File5);
+        //obj.ReadTheFile(File6);
+        //obj.ReadTheFile(File7);
+        //obj.ReadTheFile(File8);
+        //obj.ReadTheFile(File9);
+        //obj.ReadTheFile(File10);
+
+
     }
+
+    
 
    
 }
